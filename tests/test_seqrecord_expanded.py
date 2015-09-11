7cc9098c1ea405d9c63c1c8defc34aa200d972ce
carlosp420 <mycalesis@gmail.com>
2015-09-11 11:26:06 +0300

    reading frame1

--- tests/test_seqrecord_expanded.py
+++ tests/test_seqrecord_expanded.py
@@ -12,7 +12,19 @@ class TestCodonPositions(unittest.TestCase):
         self.voucher_code = 'CP100-09'
         self.taxonomy = {'genus': 'Aus', 'species': 'bus'}
 
-    def test_getting_codon_positions(self):
+    def test_getting_codon_positions_reading_frame_1(self):
+        seq = 'GAATGGAAGACAAAGTCTCGTCCA'
+        seq_record = SeqRecordExpanded(seq, reading_frame=1)
+        expected = 'GTAAATCC'
+        self.assertEqual(expected, seq_record.fist_codon_position(), 'Fist codon position')
+
+        expected = 'AGACACGC'
+        self.assertEqual(expected, seq_record.second_codon_position(), 'Second codon position')
+
+        expected = 'AGGAGTTA'
+        self.assertEqual(expected, seq_record.third_codon_position(), 'Third codon position')
+
+    def test_getting_codon_positions_reading_frame_2(self):
         seq_record = SeqRecordExpanded(self.seq, reading_frame=self.reading_frame)
         expected = 'CGGTGATAAAGCTATATGGAGAC'
         self.assertEqual(expected, seq_record.fist_codon_position(), 'Fist codon position')
