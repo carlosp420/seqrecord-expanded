7b0c1c7e75aa5409ff880df6e229c8cdf355485f
carlosp420 <mycalesis@gmail.com>
2015-09-11 11:34:35 +0300

    all reading frames

--- tests/test_seqrecord_expanded.py
+++ tests/test_seqrecord_expanded.py
@@ -5,9 +5,6 @@ from seqrecord_expanded import SeqRecordExpanded
 
 class TestCodonPositions(unittest.TestCase):
     def setUp(self):
-        self.seq = 'ACACGTCGACTCCGGCAAGTCCACTACCACAGGACATTTGATTTACAAATGTGGTGGTATCGACAAGCGT'
-        self.gene_code = 'EF1a'
-        self.reading_frame = 2
         self.table = 1  # translation table
         self.voucher_code = 'CP100-09'
         self.taxonomy = {'genus': 'Aus', 'species': 'bus'}
@@ -25,7 +22,8 @@ class TestCodonPositions(unittest.TestCase):
         self.assertEqual(expected, seq_record.third_codon_position(), 'Third codon position')
 
     def test_getting_codon_positions_reading_frame_2(self):
-        seq_record = SeqRecordExpanded(self.seq, reading_frame=self.reading_frame)
+        seq = 'ACACGTCGACTCCGGCAAGTCCACTACCACAGGACATTTGATTTACAAATGTGGTGGTATCGACAAGCGT'
+        seq_record = SeqRecordExpanded(seq, reading_frame=2)
         expected = 'CGGTGATAAAGCTATATGGAGAC'
         self.assertEqual(expected, seq_record.fist_codon_position(), 'Fist codon position')
 
@@ -33,4 +31,16 @@ class TestCodonPositions(unittest.TestCase):
         self.assertEqual(expected, seq_record.second_codon_position(), 'Second codon position')
 
         expected = 'ACCCCCGCTCAATGTCATTTCCGT'
+        self.assertEqual(expected, seq_record.third_codon_position(), 'Third codon position')
+
+    def test_getting_codon_positions_reading_frame_3(self):
+        seq = 'GTCGTGGGGGCCCACGTGGACGTGG'
+        seq_record = SeqRecordExpanded(seq, reading_frame=3)
+        expected = 'CGGCCGCG'
+        self.assertEqual(expected, seq_record.fist_codon_position(), 'Fist codon position')
+
+        expected = 'GGGGCGGGG'
+        self.assertEqual(expected, seq_record.second_codon_position(), 'Second codon position')
+
+        expected = 'TTGCATAT'
         self.assertEqual(expected, seq_record.third_codon_position(), 'Third codon position')
