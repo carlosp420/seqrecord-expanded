693a3ac8b85116f726f76b1e792f9567b320c747
carlosp420 <mycalesis@gmail.com>
2015-09-11 11:44:51 +0300

    check missing reading_frame

--- tests/test_seqrecord_expanded.py
+++ tests/test_seqrecord_expanded.py
@@ -8,6 +8,31 @@ class TestCodonPositions(unittest.TestCase):
         self.table = 1  # translation table
         self.voucher_code = 'CP100-09'
         self.taxonomy = {'genus': 'Aus', 'species': 'bus'}
+        self.seq = 'GAATGGAAGACAAAGTCTCGTCCA'
+
+    def test_missing_reading_frame(self):
+        seq_record = SeqRecordExpanded(self.seq)
+        self.assertRaises(AttributeError, seq_record.fist_codon_position)
+        self.assertRaises(AttributeError, seq_record.second_codon_position)
+        self.assertRaises(AttributeError, seq_record.third_codon_position)
+
+    def test_wrong_reading_frame_int(self):
+        seq_record = SeqRecordExpanded(self.seq, reading_frame=4)
+        self.assertRaises(ValueError, seq_record.fist_codon_position)
+        self.assertRaises(ValueError, seq_record.second_codon_position)
+        self.assertRaises(ValueError, seq_record.third_codon_position)
+
+    def test_wrong_reading_frame_str(self):
+        seq_record = SeqRecordExpanded(self.seq, reading_frame='1')
+        self.assertRaises(ValueError, seq_record.fist_codon_position)
+        self.assertRaises(ValueError, seq_record.second_codon_position)
+        self.assertRaises(ValueError, seq_record.third_codon_position)
+
+    def test_wrong_reading_frame_empty(self):
+        seq_record = SeqRecordExpanded(self.seq, reading_frame='')
+        self.assertRaises(AttributeError, seq_record.fist_codon_position)
+        self.assertRaises(AttributeError, seq_record.second_codon_position)
+        self.assertRaises(AttributeError, seq_record.third_codon_position)
 
     def test_getting_codon_positions_reading_frame_1(self):
         seq = 'GAATGGAAGACAAAGTCTCGTCCA'
