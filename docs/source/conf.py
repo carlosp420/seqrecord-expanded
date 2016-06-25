7023375022a2aa7573f44acaa7e386f40e7a8252
carlosp420 <mycalesis@gmail.com>
2016-06-25 17:13:45 -0500

    Bump version: 0.2.4 → 0.2.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ author = 'Carlos Peña'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.4'
+version = '0.2.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
