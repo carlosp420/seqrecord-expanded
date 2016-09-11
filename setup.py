a4dc1b6b38819c32c974e70e0d1245b8d90a1493
carlosp420 <mycalesis@gmail.com>
2016-09-11 15:00:48 -0500

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(*names, **kwargs):
 
 setuptools.setup(
     name="seqrecord_expanded",
-    version="0.2.5",
+    version="0.2.6",
     license="BSD",
     url="https://github.com/carlosp420/seqrecord-expanded",
 
